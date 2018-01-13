de8dc5b90572ec19a9a484d83db43cdfe6bac2ce
taramouse <support@optimized4u.org>
2018-01-13 23:28:18 +0000

    Some refactoring.

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,12 @@ import Signin from '@/components/user/Signin'
 import Record from '@/components/record/Record'
 import Stats from '@/components/stats/Stats'
 import Profiles from '@/components/profiles/Profiles'
-import NewProfile from '@/components/profiles/NewProfile'
-import DeleteProfile from '@/components/profiles/DeleteProfile'
-import DownloadProfile from '@/components/profiles/DownloadProfile'
-import EditProfile from '@/components/profiles/EditProfile'
-import ShareProfile from '@/components/profiles/ShareProfile'
-import UploadProfile from '@/components/profiles/UploadProfile'
+import NewProfile from '@/components/profiles/manage/NewProfile'
+import DeleteProfile from '@/components/profiles/manage/DeleteProfile'
+import DownloadProfile from '@/components/profiles/manage/DownloadProfile'
+import EditProfile from '@/components/profiles/manage/EditProfile'
+import ShareProfile from '@/components/profiles/manage/ShareProfile'
+import UploadProfile from '@/components/profiles/manage/UploadProfile'
 
 Vue.use(Router)
 
