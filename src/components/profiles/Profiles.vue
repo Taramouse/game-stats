95850f1b7ffe2c4893004b2bbc8d69d699c8f286
taramouse <support@optimized4u.org>
2018-01-13 18:35:13 +0000

    Changed buttons to avatars, not perfect but leaving for now. :todo issue#2

--- src/components/profiles/Profiles.vue
+++ src/components/profiles/Profiles.vue
@@ -6,13 +6,21 @@
           <v-container grid-list-lg>
             <v-layout row wrap>
               <v-flex xs4 v-for="item in menuItems" :key="item.title">
-                <v-btn
+                <!-- <v-btn
                   block
                   large
                   :to="item.link">
                   <v-icon color="blue" large>{{ item.icon }}</v-icon>
                   <span class="hidden-sm-and-down ml-2">{{ item.title }}</span>
-                </v-btn>
+                </v-btn> -->
+                <v-layout justify-space-around align-center class="mb-1">
+                    <a v-ripple :href="item.link">
+                      <v-avatar tile
+                        size="8em"
+                        :class="item.color">
+                        <v-icon dark large>{{item.icon}}</v-icon>
+                      </v-avatar></a>
+                </v-layout>
               </v-flex>
             </v-layout>
           </v-container>
@@ -26,12 +34,12 @@
     data () {
       return {
         menuItems: [
-          {icon: 'create_new_folder', title: 'New Profile', link: '/new-profile', color: 'blue'},
+          {icon: 'add', title: 'New Profile', link: '/new-profile', color: 'info'},
           {icon: 'cloud_download', title: 'Download Profile', link: '/download-profile', color: 'success'},
           {icon: 'cloud_upload', title: 'Upload Profile', link: '/share-profile', color: 'success'},
-          {icon: 'edit', title: 'Edit Profile', link: '/edit-profile', color: 'blue'},
-          {icon: 'share', title: 'Share Profile', link: '/share-profile', color: 'blue'},
-          {icon: 'delete', title: 'Delete Profile', link: '/delete-profile', color: 'danger'}
+          {icon: 'edit', title: 'Edit Profile', link: '/edit-profile', color: 'warning'},
+          {icon: 'share', title: 'Share Profile', link: '/share-profile', color: 'info'},
+          {icon: 'delete', title: 'Delete Profile', link: '/delete-profile', color: 'error'}
         ]
       }
     }
