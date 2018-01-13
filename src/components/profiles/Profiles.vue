22622ddefdcfbf8a3d7b46aa6b963fbdb55dd28a
taramouse <support@optimized4u.org>
2018-01-13 01:31:34 +0000

    Removed dummy available titles.

--- src/components/profiles/Profiles.vue
+++ src/components/profiles/Profiles.vue
@@ -1,60 +1,24 @@
-<<template>
-  <v-layout row wrap>
-    <v-flex sm12 md6 offset-md3>
-      <v-card class="grid">
-        <v-subheader>Manage Game Profiles</v-subheader>
-        <v-container grid-list-lg>
-          <v-layout row wrap>
-            <v-flex xs4 v-for="item in menuItems" :key="item.title">
-              <v-btn
-                block
-                large
-                :to="item.link">
-                <v-icon color="blue" large>{{ item.icon }}</v-icon>
-                <span class="hidden-sm-and-down ml-2">{{ item.title }}</span>
-              </v-btn>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-
-    <v-flex sm12 md6 offset-md3>
-      <v-card class="grid">
-        <v-subheader>Available Game Profiles</v-subheader>
-        <v-container grid-list-lg>
-          <v-layout row wrap>
-
-            <v-card elevation-2 color="blue-grey darken-2" class="white--text mb-2">
-              <v-card-title primary-title>
-                <div class="headline">Real Racing 3</div>
-              </v-card-title>
-              <v-card-text>
-                <p>Record your fastest laps and quickest races including car, track and event.</p>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn flat dark>Record Now</v-btn>
-              </v-card-actions>
-            </v-card>
-
-            <v-card elevation-2 color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div class="headline">War Robots</div>
-              </v-card-title>
-              <v-card-text>
-                <p>Record weapon and robot level.</p>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn flat dark>Record Now</v-btn>
-              </v-card-actions>
-            </v-card>
-
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-
-  </v-layout>
+<template>
+    <v-layout row wrap class="mt-2">
+      <v-flex sm12 md6 offset-md3>
+        <v-card class="grid">
+          <v-subheader>Manage Game Profiles</v-subheader>
+          <v-container grid-list-lg>
+            <v-layout row wrap>
+              <v-flex xs4 v-for="item in menuItems" :key="item.title">
+                <v-btn
+                  block
+                  large
+                  :to="item.link">
+                  <v-icon color="blue" large>{{ item.icon }}</v-icon>
+                  <span class="hidden-sm-and-down ml-2">{{ item.title }}</span>
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
 </template>
 
 <script>
@@ -74,9 +38,7 @@
   }
 
 </script>
-<<style scoped>
-  .menuItem {
-    font-size: 64px;
-  }
-</style>
 
+<style scoped>
+
+</style>
