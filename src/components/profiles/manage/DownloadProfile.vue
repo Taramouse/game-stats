9b4a8a707393d40c6b560f73869406301614fd24
taramouse <support@optimized4u.org>
2018-01-28 16:37:16 +0000

    Can now download profiles from Firebase and set them in Vuex store.

--- src/components/profiles/manage/DownloadProfile.vue
+++ src/components/profiles/manage/DownloadProfile.vue
@@ -1,7 +1,63 @@
 <template>
-  <v-layout justify-center align-center >
-    <div>
-      <span class="display-2">Dowload Profiles</span>
-    </div>
-  </v-layout>
+  <v-layout>
+      <v-flex xs12 md8 offset-md2>
+        <v-card>
+          <v-container fluid class="pa-0">
+            <v-layout row>
+              <v-flex xs12>
+                <v-card>
+                  <v-container fluid grid-list-sm>
+                    <v-layout row wrap v-for="profile in profiles" :key="profile.id">
+                      <v-flex xs12 class="pa-0 ma-1">
+                          <v-card-text
+                            class="primary display-3"
+                          >{{profile.name}}
+                          </v-card-text>
+                          <v-card-text
+                            class="primary darken-1 subheading"
+                          >{{profile.description}}
+                        </v-card-text>
+                        <v-card-actions>
+                          <v-btn flat>
+                            <v-icon left>cloud_download</v-icon>
+                            Download
+                          </v-btn>
+                          <v-btn flat>
+                            <v-icon left>edit</v-icon>
+                            Edit
+                          </v-btn>
+                          <v-btn flat color="error">
+                            <v-icon left>delete</v-icon>
+                            Delete
+                          </v-btn>
+                        </v-card-actions>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card>
+              </v-flex>
+            </v-layout>
+
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    created () {
+      this.$store.dispatch('loadUserProfiles')
+    },
+    computed: {
+      profiles () {
+        return this.$store.getters.getLoadedProfiles
+      }
+    }
+  }
+</script>
--- src/store/profiles/index.js
+++ src/store/profiles/index.js
@@ -3,40 +3,62 @@ import * as firebase from 'firebase'
 
 export default {
   state: {
-    profileName: 'Real Racing 3',
-    profileDescription: 'Log fastest lap and race time for tracks and cars.',
-    profileItems: [
-      { text: 'Date Recorded', value: 'date' },
+    loadedProfiles: [
+      {
+        id: 1,
+        name: 'Vuex is working!',
+        description: 'Log fastest lap and race time for tracks and cars.',
+        items: [
+          { text: 'Date Recorded', value: 'date' },
           { text: 'Fastest Lap', value: 'fastest-lap' },
           { text: 'Race Time', value: 'race-time' },
           { text: 'Circuit', value: 'circuit' },
           { text: 'Car Manufacturer', value: 'car-manufacturer' },
           { text: 'Car Model', value: 'car-model' }
-    ],
-    items: [
-      { value: false, name: 'Record 1', date: '23-01-18', 'fastest-lap': '1:30.673', 'race-time': '10:48.549', circuit: 'Spa francorchamps', 'car-manufacturer': 'Porche', 'car-model': '911 RSR 2014' },
-      { value: false, name: 'Record 2', date: '24-01-18', 'fastest-lap': '1:30.432', 'race-time': '10:28.338', circuit: 'Spa francorchamps', 'car-manufacturer': 'Porche', 'car-model': '911 RSR 2014' }
+        ]
+      }
     ]
   },
   mutations: {
-    createProfile (state, payload) {
-      // state.loadedProfile.push(payload)
+    setLoadedProfiles (state, profiles) {
+      state.loadedProfiles = profiles
+    },
+    updateProfileName (state, payload) {
+      state.loadedProfiles.name = payload
+    },
+    updateProfileDescription (state, payload) {
+      state.loadedProfiles.description = payload
+    },
+    updateProfileItems (state, payload) {
+      state.loadedProfiles.items.push(payload)
+      // state.loadedProfiles.items = [...this.loadedProfiles.items]
+    },
+    removeProfileItem (state, payload) {
+      state.loadedProfiles.items
+        .splice(state.loadedProfiles.items.indexOf(payload), 1)
+      // state.loadedProfiles.items = [...this.loadedProfiles.items]
+    },
+    // deleteTodo (state, { todo }) {
+    //   state.todos.splice(state.todos.indexOf(todo), 1)
+    // },
+    createUserProfile (state, payload) {
+      state.loadedProfiles.push(payload)
     }
   },
   actions: {
-    createProfile ({ commit, getters }, payload) {
+    createUserProfile ({ commit, getters }, payload) {
       commit('setLoading', true)
       const profile = {
-        profileName: payload.profileName,
-        profileItems: payload.profileItems,
-        profileDescription: payload.profileDescription,
+        name: payload.name,
+        description: payload.description,
+        items: payload.items,
         date: new Date().toISOString(),
         creatorId: getters.user.id
       }
-      firebase.database().ref('game-profiles').child(payload.profileName).push(profile)
+      firebase.database().ref('user-profiles').push(profile)
         .then(() => {
           commit('setLoading', false)
-          commit('createProfile', {
+          commit('createUserProfile', {
             ...profile
           })
         })
@@ -46,20 +68,19 @@ export default {
           console.log(error)
         })
     },
-    loadProfile ({commit}) {
+    loadUserProfiles ({commit}) {
       commit('setLoading', true)
       // replace once with on for realtime updates from firebase.
-      firebase.database().ref('game-profile').once('value')
+      firebase.database().ref('user-profiles').once('value')
         .then((data) => {
           const profiles = []
           const obj = data.val()
           for (let key in obj) {
             profiles.push({
               id: key,
-              profileName: obj[key].profileName,
-              prfileDescription: obj[key].profileDescription,
-              profileItems: obj[key].profileItems,
-              imageUrl: obj[key].imageUrl,
+              name: obj[key].name,
+              description: obj[key].description,
+              items: obj[key].items,
               date: obj[key].date,
               creatorId: obj[key].creatorId
             })
@@ -76,12 +97,13 @@ export default {
     }
   },
   getters: {
-    loadedProfile (state) {
-      return (profileId) => {
-        return state.loadedProfile.find((profile) => {
-          return profile.id === profileId
-        })
-      }
+    getProfileById: (state) => (id) => {
+      return state.loadedProfiles.find(loadedProfile => loadedProfile.id === id)
+    },
+    getLoadedProfiles (state) {
+      return state.loadedProfiles.sort((profileA, profileB) => {
+        return profileA.date > profileB.date
+      })
     }
   }
 }
